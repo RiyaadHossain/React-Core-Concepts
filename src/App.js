24d3af2b0f037ee176722cd75cc4e7fd6cfa2ded
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-16 21:14:55 +0600

    markup added & style changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello, React </h1>
+      <div className='card'>
+        <h2>Riyad Hossain</h2>
+        <p className='lead'>He is Slave of Allah. He want to contribute for Islam learning Web Developing.</p>
+      </div>
     </div>
   );
 }
