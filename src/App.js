0982591f59e77f452eb67dd4319f444143785402
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-16 21:33:14 +0600

    Create new componetnt and set props value dynamically

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, React </h1>
-      <div className='card'>
+      <div className="card">
         <h2>Riyad Hossain</h2>
-        <p className='lead'>He is Slave of Allah. He want to contribute for Islam learning Web Developing.</p>
+        <p className="lead">
+          He is Slave of Allah. He want to contribute for Islam learning Web
+          Developing.
+        </p>
+      </div>
+      <Compo></Compo>
+      <div className="card">
+        <People name="Riyad Hossain"></People>
+        <Friend user="Riyad" phone="01703790978"></Friend>
       </div>
     </div>
   );
 }
 
+// Component Basic
+function Compo() {
+  return <h1>Yay, New Component 😄</h1>;
+}
+
+// Component 1
+function People(props) {
+  return (
+    <div>
+      <h2>User Name: {props.name}</h2>
+    </div>
+  );
+}
+
 export default App;
+
+// Component 2
+function Friend(props) {
+  // console.log(props);
+  return (
+    <div className="container">
+      <h3>Name: {props.user}</h3>
+      <p>phone: {props.phone}</p>
+    </div>
+  );
+}
